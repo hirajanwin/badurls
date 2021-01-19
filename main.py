99c9d8c351a7b7044337f60ec389c0d631638e8f
berrysauce <contact@berrysauce.me>
2021-01-19 21:46:49 +0100

    Added Ratelimiter

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from deta import Deta
 from random import randint
 from dotenv import load_dotenv
 import os
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.errors import RateLimitExceeded
-from slowapi.util import get_remote_address
 
 
 # Setup
@@ -17,11 +14,7 @@ DETA_TOKEN = os.getenv("DETA_TOKEN")
 APP_TOKEN = os.getenv("APP_TOKEN")
 deta = Deta(DETA_TOKEN)  # configure your Deta project
 db = deta.Base("domains")  # access your DB
-
-limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
-app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
 class URLItem(BaseModel):
@@ -31,13 +24,11 @@ class URLItem(BaseModel):
 
 
 @app.get("/")
-@limiter.limit("100/second")
 def read_root():
     return {"msg": "Hello World!"}
 
 
 @app.get("/url/{urlid}")
-@limiter.limit("50/second")
 def read_item(urlid: int):
     try:
         request = next(db.fetch({"id": urlid}))[0]
@@ -47,7 +38,6 @@ def read_item(urlid: int):
 
 
 @app.get("/urls")
-@limiter.limit("50/second")
 def read_all():
     try:
         request = next(db.fetch({"show": True}))
@@ -57,7 +47,6 @@ def read_all():
 
 
 @app.post("/add")
-@limiter.limit("10/minute")
 def add_item(url: URLItem):
     if APP_TOKEN == url.token:
         rand = randint(10000, 99999)
