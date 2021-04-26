babbc5ce20ea745dc0aaa78b420cad9710c2779a
Paul <paul@berrysauce.me>
2021-04-26 22:33:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi import FastAPI, HTTPException, Request, Depends, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
